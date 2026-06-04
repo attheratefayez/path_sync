003ff24ff031325c5fb5f637e5ba7f3d7cd6627f
attheratefayez <faizurrahmman.fayez@gmail.com>
2026-06-05 00:33:08 +0600

    refactor(grid): drop SFML Drawable, plain data container with accessors

--- libs/path_sync_ui/include/path_sync_ui/grid.hpp
+++ libs/path_sync_ui/include/path_sync_ui/grid.hpp
@@ -1,13 +1,7 @@
-/**
- * @file grid.hpp
- * @brief Header file of the Grid class for Path Sync Project.
- */
-
 #ifndef __PATH_SYNC_GRID_HPP__
 #define __PATH_SYNC_GRID_HPP__
 
 #include <vector>
-#include <SFML/Graphics.hpp>
 
 #include "path_sync_core/map_loader/map_data.hpp"
 #include "path_sync_ui/cell.hpp"
@@ -15,19 +9,24 @@
 namespace path_sync
 {
 
-class Grid : public sf::Drawable
+class Grid
 {
 public:
+    Grid() = default;
     Grid(const path_sync::MapData& map_data, int cell_size);
-    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
     void sync_with_map_data(const path_sync::MapData& map_data);
 
+    int get_cell_size() const { return cell_size_; }
+    int get_width() const { return static_cast<int>(drawable_grid_.empty() ? 0 : drawable_grid_[0].size()); }
+    int get_height() const { return static_cast<int>(drawable_grid_.size()); }
+    const Cell& get_cell(int x, int y) const { return drawable_grid_[y][x]; }
+
 private:
-    int cell_size_;
+    int cell_size_ = 5;
     std::vector<std::vector<Cell>> drawable_grid_;
     void adjust_cell_size_(int height, int width);
 };
 
 } // namespace path_sync
-
-#endif // !__PATH_SYNC_GRID_HPP__
+#endif
